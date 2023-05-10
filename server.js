7a5cedca9f9b68481f93bdc8548755a0400caba9
Veronica <vveronikanika@gmail.com>
2023-05-10 22:43:47 +0300

    fix: add express.urlencoded

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const app = express();
 const port = 3000;
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use('/books', booksRouter);
 
 app.get('/', (req, res) => {
